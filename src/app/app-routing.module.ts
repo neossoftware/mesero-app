25f9ba695e4f8af35b158e8998bcbc6f92a8a5c6
neossoftware <mario.hidalgo@neosuniversity.com>
2018-01-19 02:21:53 -0600

    routes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+
+import { NgModule } from "@angular/core";
+import {Routes,RouterModule } from '@angular/router';
+
+import { ConsultaComponent } from './consulta/consulta.component';
+import { ConsumoComponent } from './consumo/consumo.component';
+import { PlatoComponent } from './plato/plato.component';
+
+
+const appRoutes: Routes = [
+{ path:'plato', component : PlatoComponent},
+{ path:'consumo', component : ConsumoComponent},
+{ path:'consulta', component : ConsultaComponent},
+{ path:'', redirectTo : 'plato', pathMatch: 'full'},
+]
+@NgModule({
+   imports : [RouterModule]
+})
+
+export class AppRoutingModule {
+
+}
