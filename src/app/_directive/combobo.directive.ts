4d5ad312761e3e7a373156b03368693cb38000c4
neossoftware <mario.hidalgo@neosuniversity.com>
2018-01-19 02:00:26 -0600

    mi cambio

---
+++ src/app/_directive/combobo.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCombobo]'
+})
+export class ComboboxDirective {
+
+  constructor() { }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { ComboboxDirective } from './_directive/combobo.directive';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ComboboDirective
   ],
   imports: [
     BrowserModule
